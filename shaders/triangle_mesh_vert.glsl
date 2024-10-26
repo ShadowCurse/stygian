d24e41862422320782b2195546cb8dcffd953b09
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2024-10-26 22:01:02 +0100

    update vertex data for quads in the shader

--- shaders/triangle_mesh_vert.glsl
+++ shaders/triangle_mesh_vert.glsl
@@ -17,42 +17,42 @@ layout(push_constant) uniform constants {
 } PushConstants;
 
 struct Vertex {
-    vec3 position;
-    float _unused;
+    vec2 position;
+    vec2 uv;
     vec4 color;
 }; 
 
 Vertex[] vertices = {
   // Top
   {
-    vec3(1.0, 1.0, 0.0),
-    0.0,
+    vec2(1.0, 1.0),
+    vec2(1.0, 0.0),
     vec4(1.0, 0.0, 0.0, 1.0),
   },
   {
-    vec3(1.0, -1.0, 0.0),
-    0.0,
+    vec2(1.0, -1.0),
+    vec2(1.0, 1.0),
     vec4(0.0, 1.0, 0.0, 1.0),
   },
   {
-    vec3(-1.0, -1.0, 0.0),
-    0.0,
+    vec2(-1.0, -1.0),
+    vec2(0.0, 1.0),
     vec4(0.0, 0.0, 1.0, 1.0),
   },
   // Bottom
   {
-    vec3(-1.0, -1.0, 0.0),
-    0.0,
+    vec2(-1.0, -1.0),
+    vec2(0.0, 1.0),
     vec4(0.0, 0.0, 1.0, 1.0),
   },
   {
-    vec3(-1.0, 1.0, 0.0),
-    0.0,
+    vec2(-1.0, 1.0),
+    vec2(0.0, 0.0),
     vec4(0.0, 1.0, 0.0, 1.0),
   },
   {
-    vec3(1.0, 1.0, 0.0),
-    0.0,
+    vec2(1.0, 1.0),
+    vec2(1.0, 0.0),
     vec4(0.0, 0.0, 1.0, 1.0),
   },
 };
@@ -61,7 +61,7 @@ void main() {
     Vertex v = vertices[gl_VertexIndex];
     QuadInfo qi = PushConstants.instance_infos.infos[gl_InstanceIndex];
 
-    vec4 new_position = qi.transform * vec4(v.position, 1.0);
+    vec4 new_position = qi.transform * vec4(v.position, 1.0, 1.0);
     gl_Position = vec4(new_position.xy, 1.0, 1.0);
 
     outColor = v.color.xyz;
