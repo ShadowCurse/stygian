8387dfa482f917e7a934a1c597839ef27ba8f235
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2024-12-24 22:34:09 +0000

    specify a lib to link for wasm

--- wasm.sh
+++ wasm.sh
@@ -12,6 +12,6 @@ emcc \
   -sALLOW_MEMORY_GROWTH=1 \
   -sSTACK_SIZE=1mb \
   --embed-file assets@/assets \
-  ../zig-out/lib/* \
+  ../zig-out/lib/libstygian_unibuild_software_emscripten.a \
   -o \
   stygian.js
